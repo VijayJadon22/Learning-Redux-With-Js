c7cb19dbcece70ec9da0d2100baecd2d63ef95a5
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-06 19:28:27 +0530

    implemented notes app reducers and actions

---
+++ todo-app/src/redux/actions/notesActions.js
@@ -0,0 +1,8 @@
+const ADD_NOTE = "ADD_NOTE";
+const DELETE_NOTE = "DELETE_NOTE";
+
+//create actions
+const addNote = (text) => ({ text, type: ADD_NOTE });
+const deleteNote = (index) => ({ index, type: DELETE_NOTE });
+
+export { ADD_NOTE, DELETE_NOTE, addNote, deleteNote };
---
+++ todo-app/src/redux/reducers/notesReducers.js
@@ -0,0 +1,29 @@
+//initial state
+
+import { ADD_NOTE, DELETE_NOTE } from "../actions/notesActions";
+
+const initialState = {
+    notes: []
+}
+
+export const noteReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_NOTE:
+            return {
+                ...state,
+                notes: [
+                    ...state.notes,
+                    { text: action.text }
+                ]
+            }
+
+        case DELETE_NOTE:
+            return {
+                ...state,
+                notes: state.notes.filter((note, i) => i !== action.index)
+            }
+
+        default:
+            return state;
+    }
+}
