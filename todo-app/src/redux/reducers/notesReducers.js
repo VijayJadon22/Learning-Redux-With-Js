d975ba46d75b38a765904cbc945aa65e97313501
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-02-08 16:02:40 +0530

    Created noteSlice with functions in noteApp part

--- todo-app/src/redux/reducers/notesReducers.js
+++ todo-app/src/redux/reducers/notesReducers.js
@@ -1,27 +1,52 @@
-import { ADD_NOTE, DELETE_NOTE } from "../actions/notesActions";
+// import { ADD_NOTE, DELETE_NOTE } from "../actions/notesActions";
+import { createSlice } from "@reduxjs/toolkit";
 
+// Define the initial state 
 const initialState = {
-    notes: []
+    notes: [{ text: "my first note" }]
 }
 
-export const noteReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_NOTE:
-            return {
-                ...state,
-                notes: [
-                    ...state.notes,
-                    { text: action.text }
-                ]
-            }
-
-        case DELETE_NOTE:
-            return {
-                ...state,
-                notes: state.notes.filter((note, i) => i !== action.index)
-            }
-
-        default:
-            return state;
+// Create a slice of the store for notes
+export const noteSlice = createSlice({
+    name: "note",
+    initialState: initialState,
+    reducers: {
+        // Reducer to add a new note to the notes array
+        add: (state, action) => {
+            state.notes.push({ text: action.payload })
+        },
+        // Reducer to delete a note from the notes array based on index
+        deleteNote: (state, action) => {
+            state.notes = state.notes.filter((note, i) => i !== action.payload)
+        }
     }
-}
+});
+
+// Export the actions to be used in the application
+export const { add, deleteNote } = noteSlice.actions;
+// Export the reducer to be included in the store
+export default noteSlice.reducer;
+
+
+
+// export const noteReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case ADD_NOTE:
+//             return {
+//                 ...state,
+//                 notes: [
+//                     ...state.notes,
+//                     { text: action.text }
+//                 ]
+//             }
+
+//         case DELETE_NOTE:
+//             return {
+//                 ...state,
+//                 notes: state.notes.filter((note, i) => i !== action.index)
+//             }
+
+//         default:
+//             return state;
+//     }
+// }
